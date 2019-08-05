1dd41246a05e16f04fd7bdf6d33bde3567cd82ef
Pedro Cristofoli <cristofolipedro@gmail.com>
2019-08-05 17:18:29 -0300

    começo perfil serviço

---
+++ view/perfil_servico.php
@@ -0,0 +1,75 @@
+<?php //include_once 'include/verificaOrganizador.php';?>
+<?php include_once 'include/banco.php';?>
+<?php
+    include_once '../autoload.php'; 
+	if($_GET['servico']){ // Caso os dados sejam enviados via GET
+
+        $servicoControle = new ControleServico();
+        //Passa o GET desta View para o Controle
+        $servicoControle->setVisao($_GET);
+    
+        $servicoUnico = $servicoControle->controleAcao("listarUnico", $_GET["servico"]);  //value="<?= isset($categoriaAlteracao) ? $categoriaAlteracao->getId() : "";
+    }
+?>
+<!DOCTYPE html>
+<html>
+<?php include_once('include/head.php'); ?>
+<body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- jQuery -->
+    <script src="js/jquery.js" crossorigin="anonymous"></script>
+	<!-- Meu js -->
+	<script src="js/main.js"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    
+
+    <?php include_once('include/navbar.php'); ?>
+
+    <div class="wrapper">
+
+    	<?php include_once('include/sidebar.php'); ?>
+
+    	<div id="page">
+			<div class="filtros">Edição do evento</div>
+                        <!-- Page Content -->
+                        <div class="content co-10 normal-shadow">
+                            <input type="hidden" id="idServico" name="idServico" value="<?= isset($servicoUnico) ? $servicoUnico->getId() : "";?>"/>
+                            <div style="float: right; display: none">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                    <div class="input-group input-group-alternative">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
+                                        </div>
+                                        <input class="form-control-alternative datepicker" placeholder="Select date" type="text" value="06/20/2019">
+                                    </div>
+                                </div>
+                                </div>
+                                
+                            </div>
+                            <div class="content-header" style="float: left;">
+                                <div class="header-photo alternative-shadow">
+                                    <img src="img/blog-neon-6.jpg">
+                                </div>
+                            </div>
+                            <div class="col-lg-12" style="margin-top: 20px;">
+                                <div class="form-group">
+                                    <input type="text" style="width: 300px !important;" id="input-nome" class="form-control form-control-alternative form-edita form-title" placeholder="First name" value="<?= isset($servicoUnico) ? $servicoUnico->getNome() : "";?>">
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <textarea id="input-descricao" style="resize: none; width: 300px !important;" class="form-control form-control-alternative form-edita" placeholder="Adicione aqui a descrição do seu evento"><?= isset($servicoUnico) ? $servicoUnico->getCnpj() : "";?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        
+		</div>
+    </div>
+	
+</body>
+</html>
