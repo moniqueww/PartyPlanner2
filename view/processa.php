3f78c3f619ca48ffbfc0dc0b5970bb8340f0f7c8
lorenzomene <lorenzospmene@gmail.com>
2019-08-13 21:00:56 -0300

    estrelinhas cadastrando

--- view/processa.php
+++ view/processa.php
@@ -9,18 +9,18 @@ if(!empty($_POST['estrela'])){
 	$estrela = $_POST['estrela'];
 	$id = $_POST['id_servico'];
 	//Salvar no banco de dados
-	$result_avaliacos = "INSERT INTO estrelas (id_servico, qnt_estrela) VALUES ('$id','$estrela')";
-	$resultado_avaliacos = mysqli_query($conn, $result_avaliacos);
+	$result_avaliacos = "INSERT INTO estrelas (id_servico, qnt_estrelas) VALUES ('$id','$estrela')";
+	$resultado_avaliacos = mysqli_query($conexao, $result_avaliacos);
 	
-	if(mysqli_insert_id($conn)){
+	if(mysqli_insert_id($conexao)){
 		$_SESSION['msg'] = "Avaliação cadastrada com sucesso";
 		header("Location: lista_servico.php");
 	}else{
 		$_SESSION['msg'] = "Erro ao cadastrar a avaliação";
-		header("Location: lista_servico.php");
+		header("Location: login.php");
 	}
 	
 }else{
 	$_SESSION['msg'] = "Necessário selecionar pelo menos 1 estrela";
-	header("Location: lista_servico.php");
+	header("Location: login.php");
 }
